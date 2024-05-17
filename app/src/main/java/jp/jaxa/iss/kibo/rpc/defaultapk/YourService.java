007c878121260e1dc7d57b8d7fc3b142d7ef5ce7
MyNameTsThad <pvpthadgaming@gmail.com>
2024-05-17 20:09:39 +0700

    Initial commit

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -0,0 +1,27 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+
+import org.opencv.core.Mat;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee.
+ */
+
+public class YourService extends KiboRpcService {
+    @Override
+    protected void runPlan1(){
+
+    }
+
+    @Override
+    protected void runPlan2(){
+    }
+
+    @Override
+    protected void runPlan3(){
+    }
+}
