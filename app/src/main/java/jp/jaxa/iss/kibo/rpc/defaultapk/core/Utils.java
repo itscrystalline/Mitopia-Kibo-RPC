2be411c0d9c46eba0b3738aeccf44945bc4d0534
MyNameTsThad <pvpthadgaming@gmail.com>
2024-05-21 20:54:51 +0700

    core: added javadoc core: added eulerAnglesDeg function

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/core/Utils.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/core/Utils.java
@@ -3,7 +3,14 @@ package jp.jaxa.iss.kibo.rpc.defaultapk.core;
 import gov.nasa.arc.astrobee.types.Quaternion;
 
 public class Utils {
-    public static Quaternion eulerAngles(float roll, float pitch, float yaw) {
+    /**
+     * Create a quaternion from Euler angles
+     * @param roll (X) the roll angle in radians
+     * @param pitch (Y) the pitch angle in radians
+     * @param yaw (Z) the yaw angle in radians
+     * @return the quaternion
+     */
+    public static Quaternion eulerAnglesRad(float roll, float pitch, float yaw) {
         float cr = (float) Math.cos(roll * 0.5);
         float sr = (float) Math.sin(roll * 0.5);
         float cp = (float) Math.cos(pitch * 0.5);
@@ -17,5 +24,16 @@ public class Utils {
           cr * cp * sy - sr * sp * cy,
           cr * cp * cy + sr * sp * sy
         );
+    }
+
+    /**
+     * Create a quaternion from Euler angles
+     * @param roll (X) the roll angle in degrees
+     * @param pitch (Y) the pitch angle in degrees
+     * @param yaw (Z) the yaw angle in degrees
+     * @return the quaternion
+     */
+    public static Quaternion eulerAnglesDeg(float roll, float pitch, float yaw) {
+        return eulerAnglesRad((float) Math.toRadians(roll), (float) Math.toRadians(pitch), (float) Math.toRadians(yaw));
     }
 }
