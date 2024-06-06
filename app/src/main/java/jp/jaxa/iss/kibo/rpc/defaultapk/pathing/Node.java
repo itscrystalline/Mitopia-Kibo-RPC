c6305bf36fff2ccbb72568afc16db2b900113508
Beyond227 <100117290+BigBoyBoba@users.noreply.github.com>
2024-06-06 22:09:20 +0700

    Create Node.java

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/pathing/Node.java
@@ -0,0 +1,47 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk.pathing;
+
+import jp.jaxa.iss.kibo.rpc.defaultapk.pathing.Point;
+import jp.jaxa.iss.kibo.rpc.defaultapk.subsystem.MapPositionManager;
+
+import java.util.ArrayList;
+
+public class Node {
+    public Point Point;
+    public ArrayList<EdgesInfo>  Edges;
+
+    public Node(double x, double y, double z){
+        Point p = new Point(x,y,z);
+        if(isValidNode(p)) {
+            Point = p;
+        }
+    }
+
+    public Node(Point p){
+        if(isValidNode(p)){
+            Point = p;
+        }
+    }
+
+    public boolean isValidNode(Point p){
+        int cnt = 0;
+        for(BoundingBox b: MapPositionManager.KIZs){
+            double[] Position = p.toArray();
+            if(!(b.contains(Position[0],Position[1],Position[2]))){
+                cnt++;
+            }
+        }
+        if(cnt==2){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public void AddEdgeList(Point dest,double Dist){
+        Edges.add(new EdgesInfo(dest,Dist));
+    }
+    public void AddEdgeList(Point dest){
+        double distance = Math.sqrt(Math.pow(Point.getX() - dest.getX(), 2) + Math.pow(Point.getY() - dest.getY(), 2) + Math.pow(Point.getZ() - dest.getZ(), 2));
+        Edges.add(new EdgesInfo(dest,distance));
+    }
+}
