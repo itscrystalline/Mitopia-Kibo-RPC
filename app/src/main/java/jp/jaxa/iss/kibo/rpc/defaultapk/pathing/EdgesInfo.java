1b89131eb47d82ee4b7288daf6ee78572313259f
Beyond227 <worapol951@gmail.com>
2024-06-09 20:52:14 +0700

    Change EdgesInfo class to store node, weight instead of point, weight

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/pathing/EdgesInfo.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/pathing/EdgesInfo.java
@@ -2,10 +2,10 @@ package jp.jaxa.iss.kibo.rpc.defaultapk.pathing;
 
 public class EdgesInfo {
     public double Distance; //source to dest dist
-    public Point DesPosition;
+    public Node DestNode;
 
-    public EdgesInfo(Point desPosition,double weight){
-        DesPosition = desPosition;
+    public EdgesInfo(Node destNode,double weight){
+        DestNode = destNode;
         Distance = weight;
     }
 }
