f131fdc03ffce98328ac607adf10d8483257ee91
Beyond227 <worapol951@gmail.com>
2024-06-19 10:26:48 +0700

    add image collection path

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/pathing/ImageCollectionPath.java
@@ -0,0 +1,32 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk.pathing;
+
+import android.util.Log;
+
+import gov.nasa.arc.astrobee.types.Quaternion;
+import jp.jaxa.iss.kibo.rpc.defaultapk.core.Astro;
+import jp.jaxa.iss.kibo.rpc.defaultapk.core.EulerAngles;
+import jp.jaxa.iss.kibo.rpc.defaultapk.subsystem.MapPositionManager;
+
+
+public class ImageCollectionPath {
+
+    // I found these points by testing in the place
+    public static Point[] ScanNode = {
+            new Point(10.95d,-9.78d,5.195d),
+            new Point(11.05d,-8.875d,4.76d),
+            new Point(11.05d,-7.925d,4.76d),
+            new Point(11d,-6.8525d,4.945d)
+    };
+
+    public static void Start(){
+        Node prevArea = Graph.addNode(Astro.bee.getPosition());
+        for(int i =0;i<4;i++){
+            Node Area = Graph.addNode(ScanNode[i]);
+            Path path = Path.dijkstra(prevArea,Area);
+            path.debugDijkstra();
+            path.moveByPath();
+            prevArea = Area;
+            Log.i("ScanPath","AREA: "+(i+1)+" is finished");
+        }
+    }
+}
